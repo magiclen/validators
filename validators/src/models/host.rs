a97b81b5c4bd19e25703de2631752129a50081d9
Magic Len <len@magiclen.org>
2020-07-17 02:26:08 +0800

    fix comments

--- validators/src/models/host.rs
+++ validators/src/models/host.rs
@@ -4,7 +4,7 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Host {
-    // non-fully qualified and ascii-encoded domain name (suffixed with a dot)
+    /// non-fully qualified and ascii-encoded domain name
     Domain(String),
     IPv4(Ipv4Addr),
     IPv6(Ipv6Addr),
