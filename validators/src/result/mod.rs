ea11679ef20a26b26af358ed5580376a4dead3f2
Magic Len <len@magiclen.org>
2021-05-02 01:38:14 +0800

    improve the `Result` struct

--- validators/src/result/mod.rs
+++ validators/src/result/mod.rs
@@ -1,5 +1,7 @@
 use crate::rocket;
 
+use std::ops::Deref;
+
 use rocket::form::{self, FromFormField, ValueField};
 use rocket::request::FromParam;
 
@@ -14,6 +16,11 @@ impl<T, E> Result<T, E> {
     pub fn into_std_result(self) -> std::result::Result<T, E> {
         self.0
     }
+
+    #[inline]
+    pub fn as_std_result(&self) -> &std::result::Result<T, E> {
+        &self.0
+    }
 }
 
 impl<T, E> From<std::result::Result<T, E>> for Result<T, E> {
@@ -27,6 +34,15 @@ impl<T, E> From<Result<T, E>> for std::result::Result<T, E> {
     #[inline]
     fn from(result: Result<T, E>) -> Self {
         result.0
+    }
+}
+
+impl<T, E> Deref for Result<T, E> {
+    type Target = std::result::Result<T, E>;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
