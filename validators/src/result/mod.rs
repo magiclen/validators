5a239d0882cd33738d59a00310e96640aecffc7a
Magic Len <len@magiclen.org>
2021-05-01 23:58:02 +0800

    improve the `Result` struct

--- validators/src/result/mod.rs
+++ validators/src/result/mod.rs
@@ -3,29 +3,30 @@ use crate::rocket;
 use rocket::form::{self, FromFormField, ValueField};
 use rocket::request::FromParam;
 
+/// A wrapper of `std::result::Result`, in order to implement the `FromFormField` trait of Rocket.
+///
+/// This struct uses the `FromParam` trait to implement the `FromFormField` trait (only impl the `from_value` method) so that it can remain the error type for checking later.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum Result<T, E> {
-    Ok(T),
-    Err(E),
+pub struct Result<T, E>(std::result::Result<T, E>);
+
+impl<T, E> Result<T, E> {
+    #[inline]
+    pub fn into_std_result(self) -> std::result::Result<T, E> {
+        self.0
+    }
 }
 
 impl<T, E> From<std::result::Result<T, E>> for Result<T, E> {
     #[inline]
     fn from(result: std::result::Result<T, E>) -> Self {
-        match result {
-            Ok(v) => Result::Ok(v),
-            Err(v) => Result::Err(v),
-        }
+        Self(result)
     }
 }
 
 impl<T, E> From<Result<T, E>> for std::result::Result<T, E> {
     #[inline]
     fn from(result: Result<T, E>) -> Self {
-        match result {
-            Result::Ok(v) => Ok(v),
-            Result::Err(v) => Err(v),
-        }
+        result.0
     }
 }
 
@@ -34,6 +35,6 @@ impl<'v, E: Sized + Send, T: FromParam<'v, Error = E> + Send> FromFormField<'v>
 {
     #[inline]
     fn from_value(v: ValueField<'v>) -> form::Result<'v, Self> {
-        Ok(Result::from(<T as FromParam>::from_param(v.value)))
+        Ok(<T as FromParam>::from_param(v.value).into())
     }
 }
