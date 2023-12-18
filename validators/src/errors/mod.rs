41d95528d89d3e3f79b86a73a4282f75262ec21f
Magic Len <len@magiclen.org>
2023-12-18 13:21:26 +0800

    add self to prevent ambiguous issues

--- validators/src/errors/mod.rs
+++ validators/src/errors/mod.rs
@@ -1,122 +1,122 @@
 #[cfg(feature = "base32")]
 mod base32;
 #[cfg(feature = "base32")]
-pub use base32::*;
+pub use self::base32::*;
 
 #[cfg(feature = "base32_decoded")]
 mod base32_decoded;
 #[cfg(feature = "base32_decoded")]
-pub use base32_decoded::*;
+pub use self::base32_decoded::*;
 
 #[cfg(feature = "base64")]
 mod base64;
 #[cfg(feature = "base64")]
-pub use base64::*;
+pub use self::base64::*;
 
 #[cfg(feature = "base64_decoded")]
 mod base64_decoded;
 #[cfg(feature = "base64_decoded")]
-pub use base64_decoded::*;
+pub use self::base64_decoded::*;
 
 #[cfg(feature = "base64_url")]
 mod base64_url;
 #[cfg(feature = "base64_url")]
-pub use base64_url::*;
+pub use self::base64_url::*;
 
 #[cfg(feature = "base64_url_decoded")]
 mod base64_url_decoded;
 #[cfg(feature = "base64_url_decoded")]
-pub use base64_url_decoded::*;
+pub use self::base64_url_decoded::*;
 
 #[cfg(feature = "bit")]
 mod bit;
 #[cfg(feature = "bit")]
-pub use bit::*;
+pub use self::bit::*;
 
 #[cfg(feature = "boolean")]
 mod boolean;
 #[cfg(feature = "boolean")]
-pub use boolean::*;
+pub use self::boolean::*;
 
 #[cfg(feature = "byte")]
 mod byte;
 #[cfg(feature = "byte")]
-pub use byte::*;
+pub use self::byte::*;
 
 #[cfg(feature = "domain")]
 mod domain;
 #[cfg(feature = "domain")]
-pub use domain::*;
+pub use self::domain::*;
 
 #[cfg(feature = "email")]
 mod email;
 #[cfg(feature = "email")]
-pub use email::*;
+pub use self::email::*;
 
 #[cfg(feature = "host")]
 mod host;
 #[cfg(feature = "host")]
-pub use host::*;
+pub use self::host::*;
 
 #[cfg(feature = "http_url")]
 mod http_url;
 #[cfg(feature = "http_url")]
-pub use http_url::*;
+pub use self::http_url::*;
 
 #[cfg(feature = "http_ftp_url")]
 mod http_ftp_url;
 #[cfg(feature = "http_ftp_url")]
-pub use http_ftp_url::*;
+pub use self::http_ftp_url::*;
 
 #[cfg(feature = "ip")]
 mod ip;
 #[cfg(feature = "ip")]
-pub use ip::*;
+pub use self::ip::*;
 
 #[cfg(feature = "ipv4")]
 mod ipv4;
 #[cfg(feature = "ipv4")]
-pub use ipv4::*;
+pub use self::ipv4::*;
 
 #[cfg(feature = "ipv6")]
 mod ipv6;
 #[cfg(feature = "ipv6")]
-pub use ipv6::*;
+pub use self::ipv6::*;
 
 #[cfg(feature = "json")]
 mod json;
 #[cfg(feature = "json")]
-pub use json::*;
+pub use self::json::*;
 
 #[cfg(feature = "length")]
 mod length;
 #[cfg(feature = "length")]
-pub use length::*;
+pub use self::length::*;
 
 #[cfg(feature = "line")]
 mod line;
 #[cfg(feature = "line")]
-pub use line::*;
+pub use self::line::*;
 
 #[cfg(feature = "mac_address")]
 mod mac_address;
 #[cfg(feature = "mac_address")]
-pub use mac_address::*;
+pub use self::mac_address::*;
 
 #[cfg(feature = "number")]
 mod number;
 #[cfg(feature = "number")]
-pub use number::*;
+pub use self::number::*;
 
 #[cfg(feature = "phone")]
 mod phone;
 #[cfg(feature = "phone")]
-pub use phone::*;
+pub use self::phone::*;
 
 #[cfg(feature = "regex")]
 mod regex;
 #[cfg(feature = "regex")]
-pub use regex::*;
+pub use self::regex::*;
 
 #[cfg(any(feature = "semver", feature = "semver_req"))]
 mod semver;
@@ -126,17 +126,17 @@ pub use self::semver::*;
 #[cfg(feature = "signed_integer")]
 mod signed_integer;
 #[cfg(feature = "signed_integer")]
-pub use signed_integer::*;
+pub use self::signed_integer::*;
 
 #[cfg(feature = "text")]
 mod text;
 #[cfg(feature = "text")]
-pub use text::*;
+pub use self::text::*;
 
 #[cfg(feature = "unsigned_integer")]
 mod unsigned_integer;
 #[cfg(feature = "unsigned_integer")]
-pub use unsigned_integer::*;
+pub use self::unsigned_integer::*;
 
 #[cfg(feature = "url")]
 mod url;
@@ -146,4 +146,4 @@ pub use self::url::*;
 #[cfg(feature = "uuid")]
 mod uuid;
 #[cfg(feature = "uuid")]
-pub use uuid::*;
+pub use self::uuid::*;
